2bd7edf62b2559889c62d53b2a79bc3d1e783439
Arraziii <arrazisyakir@gmail.com>
2025-01-10 19:21:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+document.querySelectorAll('.item').forEach(item => {
+    observer.observe(item);
+});
